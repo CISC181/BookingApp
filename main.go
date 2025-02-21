56fb285195130ae947ce03cad3db1a8814d5d4f0
CISC181 <bgibbons@udel.edu>
2025-02-21 15:10:15 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// Initialize Project
+//	go mod init bookingApp
+
+//	Execute program
+//	go run main.go
+
+//	func main() is the entry point
+func main() {
+
+	//var conferenceName = "Go Conference"
+	//	This is how you create a variable and assign without using var
+
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	//fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	//	ask user for their name
+	//	the & is using the variable pointer
+	//	This will push the value entered into the pointer for &firstName
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+
+	//	Print the memory pointer
+	fmt.Println(&firstName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+}
